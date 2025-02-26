a42fc966005207b405a923c8efe19d36ae935129
Robin Pohlman <robin.pohlman@gmail.com>
2025-02-26 12:32:45 +0100

    feat: add Header component and update page layout; clean up README and .gitignore

---
+++ src/app/components/header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="w-full p-4 bg-gray-800 text-white">
+      <h1 className="text-xl">Task Manager</h1>
+    </header>
+  );
+};
+
+export default Header;
