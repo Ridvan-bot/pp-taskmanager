65c91f3f0410452e446ce0bbb862b683f9a6d19b
Robin Pohlman <robin.pohlman@gmail.com>
2025-02-27 09:57:08 +0100

    feat: enhance Header with customer selection and login button; update global styles for improved UI

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -3,15 +3,22 @@ import Image from 'next/image';
 
 const Header = () => {
   return (
-    <header className="w-full p-4 text-white flex items-center justify-between border-b border-gray-800">
+    <header className="w-full p-4 text-white flex">
       <div className="flex items-center">
         <Image src="/pohlmanproteanab.png" alt="Logo" width={40} height={40} />
-        <h1 className="text-s ml-4">Pohlman Protean Task Managerr</h1>
+        <h1 className="text-s ml-4">Pohlman Protean Task Manager</h1>
       </div>
-      <nav>
-        <ul className="flex space-x-4">
-        </ul>
-      </nav>
+      <div className="flex-grow flex justify-center select-customer ">
+        <select className="p-3 px-20">
+        <option value="" disabled selected hidden>Select customer</option>
+          <option value="Aspia">Aspia</option>
+          <option value="Lantmännen">Lantmännen</option>
+          <option value="Kvadrat">Kvadrat</option>
+        </select>
+      </div>
+      <div className="flex items-center">
+        <button className="p-3 px-6 rounded-lg pohlman-button">Login</button>
+      </div>
     </header>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,14 @@ import ChangeName from "./components/changeName";
 
 export default function Home() {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <Header />
-      <div className="grid grid-rows-[1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <div className="flex-grow grid grid-rows-[1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <main className="flex flex-col gap-8 row-start-1 items-center sm:items-start">
           <ChangeName />
         </main>
       </div>
       <Footer />
-    </>
+    </div>
   );
 }
