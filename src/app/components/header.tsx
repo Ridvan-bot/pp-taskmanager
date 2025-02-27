f56f7f80f926323186951d0ed95e5e6be27dada0
Robin Pohlman <robin.pohlman@gmail.com>
2025-02-27 08:39:13 +0100

    feat: add ChangeName component; update Header with logo and new layout; modify global styles and metadata

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import Image from 'next/image';
 
 const Header = () => {
   return (
-    <header className="w-full p-4 bg-gray-800 text-white">
-      <h1 className="text-xl">Task Manager Header</h1>
+    <header className="w-full p-4 text-white flex items-center justify-between border-b border-gray-800">
+      <div className="flex items-center">
+        <Image src="/pohlmanproteanab.png" alt="Logo" width={40} height={40} />
+        <h1 className="text-s ml-4">Pohlman Protean Task Managerr</h1>
+      </div>
+      <nav>
+        <ul className="flex space-x-4">
+        </ul>
+      </nav>
     </header>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
-import Image from "next/image";
 import Header from "./components/header";
 import Footer from "./components/footer";
+import ChangeName from "./components/changeName";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <>
       <Header />
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-      </main>
+      <div className="grid grid-rows-[1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <main className="flex flex-col gap-8 row-start-1 items-center sm:items-start">
+          <ChangeName />
+        </main>
+      </div>
       <Footer />
-    </div>
+    </>
   );
 }
